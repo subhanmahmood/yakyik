98f25058263c45ab391857cde35668ff686a5d55
Subhan Mahmood <sm28105@woking.ac.uk>
2017-08-24 14:37:33 +0100

    init

---
+++ controllers/ZoneController.js
@@ -0,0 +1,45 @@
+var Zone = require( '../models/Zone' );
+
+module.exports = {
+  find: function( params, callback ) {
+    Zone.find( params, function( err, zones ) {
+      if ( err ) {
+        callback( err, null )
+        return
+      }
+
+      callback( null, zones )
+    })
+  },
+  findById: function( id, callback ) {
+    Zone.findById( id, function( err, zone ) {
+      if ( err ) {
+        callback( err, null )
+        return
+      }
+
+      callback( null, zone )
+    })
+  },
+  update: function( ) {},
+  create: function( params, callback) {
+    var zips = params['zipCodes']
+    var zip = zips.split(',');
+    var newZips = []
+    zip.forEach(function(zipCode){
+      newZips.push(zipCode.trim())
+    })
+
+    params['zipCodes'] = newZips 
+
+    Zone.create(params, function(err, zone){
+      if(err){
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+  destroy: function( ) {}
+}
